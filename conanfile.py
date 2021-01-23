1e865329efb58c2e0f32aeb38422db8874c7976e
Christoph Winter <mail.christoph.winter@gmail.com>
2021-01-23 13:42:49 +0100

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conans import ConanFile, CMake, tools
+
+
+class FuseppConan(ConanFile):
+    name = "fusepp"
+    version = "3.0.0"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Fusepp here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        self.run("git clone https://github.com/winternet/fusepp.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="fusepp")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="fusepp")
+        self.copy("Fuse.cpp", dst="include", src="fusepp")
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["fusepp"]
+
