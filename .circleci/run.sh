8ba28fd398bb4483e0a3fd824bf867afc746ef1f
Christoph Winter <mail.christoph.winter@gmail.com>
2021-01-24 12:07:20 +0100

    replace py helper with plain bash script

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,5 +4,7 @@
 set -e
 set -x
 
-#conan create .
-python build.py
+conan remote add conan-priv "https://api.bintray.com/conan/winternet/${CONAN_REMOTE}"
+conan user -p ${CONAN_API_KEY} -r ${CONAN_REMOTE} ${CONAN_USER}
+conan create . fusepp/3.0.0@winternet/testing
+conan upload fusepp/3.0.0@winternet/testing -r ${CONAN_REMOTE}
